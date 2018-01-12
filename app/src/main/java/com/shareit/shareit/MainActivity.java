a20a12d5eb764781fda63199fd0936a22a9c4dce
IVAN GABRIEL PAJON RODRIGUEZ <21609121@alumnos.uem.es>
2018-01-12 11:07:02 +0100

    Created project

---
+++ app/src/main/java/com/shareit/shareit/MainActivity.java
@@ -0,0 +1,13 @@
+package com.shareit.shareit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
