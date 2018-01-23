6fe6323fbcefd1ff2c509728ad44a64298e697ba
Cristhian20010 <cristhiancabrera.smr@gmail.com>
2018-01-23 13:33:46 +0100

    pantalla login

--- app/src/main/java/com/shareit/shareit/MainActivity.java
+++ app/src/main/java/com/shareit/shareit/MainActivity.java
@@ -1,13 +1,27 @@
 package com.shareit.shareit;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText usuario;
+    EditText password;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        usuario=(EditText)findViewById(R.id.usuario);
+        password=(EditText)findViewById(R.id.password);
+
+    }
+
+    public void Acceder(View v){
+        
     }
 }
