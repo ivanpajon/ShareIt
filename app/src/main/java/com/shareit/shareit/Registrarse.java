c351782466c775b1f9ad436c6880d8982025dce3
Cristhian20010 <cristhiancabrera.smr@gmail.com>
2018-01-26 12:03:53 +0100

    pantalla login

--- app/src/main/java/com/shareit/shareit/Registrarse.java
+++ app/src/main/java/com/shareit/shareit/Registrarse.java
@@ -1,7 +1,8 @@
 package com.shareit.shareit;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class Registrarse extends AppCompatActivity {
 
@@ -9,5 +10,15 @@ public class Registrarse extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrarse);
+    }
+
+    public void AccederConGoogle(View v){
+
+    }
+    public void AccederConFacebook(View v){
+
+    }
+    public void AccederConEmail(View v){
+
     }
 }
