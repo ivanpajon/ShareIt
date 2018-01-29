c2ebf354f21771807861cc36b4e58d5fed1977d6
mario <mario.g.a.90@gmail.com>
2018-01-29 13:44:57 +0100

    cambios titulo

--- app/src/main/java/com/shareit/shareit/HomeActivity.java
+++ app/src/main/java/com/shareit/shareit/HomeActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.widget.SearchView;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,6 +16,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.shareit.shareit.fragment.AcercaDeFragment;
 import com.shareit.shareit.fragment.AjustesFragment;
@@ -72,6 +75,7 @@ public class HomeActivity extends AppCompatActivity
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.home, menu);
+
         return true;
     }
 
