66810aae9f2ff0620ed37388e59939e0f1de8cc6
mario <mario.g.a.90@gmail.com>
2018-02-09 13:35:11 +0100

    cargar drawer

--- app/src/main/java/com/shareit/shareit/Home.java
+++ app/src/main/java/com/shareit/shareit/Home.java
@@ -1,5 +1,4 @@
 package com.shareit.shareit;
-
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
--- app/src/main/java/com/shareit/shareit/fragment/ComunidadesFragment.java
+++ app/src/main/java/com/shareit/shareit/fragment/ComunidadesFragment.java
@@ -4,50 +4,36 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.shareit.shareit.R;
+import com.shareit.shareit.adapters.AdapterRecyclerDemandas;
+import com.shareit.shareit.model.Demanda;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ComunidadesFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ComunidadesFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+
+
 public class ComunidadesFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     private OnFragmentInteractionListener mListener;
+    RecyclerView recyclerDemandas;
+    AdapterRecyclerDemandas adapter;
+    LinearLayoutManager llm;
+    ArrayList<Demanda> lista;
 
     public ComunidadesFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ComunidadesFragment.
-     */
     // TODO: Rename and change types and number of parameters
     public static ComunidadesFragment newInstance(String param1, String param2) {
         ComunidadesFragment fragment = new ComunidadesFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -55,17 +41,42 @@ public class ComunidadesFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_comunidades, container, false);
+       View view = inflater.inflate(R.layout.fragment_comunidades, container, false);
+        lista = new ArrayList<>();
+        recyclerDemandas = view.findViewById(R.id.recyclerDemandas);
+        recyclerDemandas.setHasFixedSize(true);
+        llm = new LinearLayoutManager(getContext());
+        recyclerDemandas.setLayoutManager(llm);
+
+        //Generamos el adaptador
+        adapter = new AdapterRecyclerDemandas(lista);
+        recyclerDemandas.setAdapter(adapter);
+        llenarLista();
+
+
+        return view;
+    }
+
+    private void llenarLista() {
+        lista.add(new Demanda("nombre","descripcion",R.drawable.persona));
+        lista.add(new Demanda("nombre 1","descripcion 1",R.drawable.persona));
+        lista.add(new Demanda("nombre 2","descripcion 2",R.drawable.persona));
+        lista.add(new Demanda("nombre 3","descripcion 3",R.drawable.persona));
+        lista.add(new Demanda("nombre 3","descripcion 3",R.drawable.persona));
+        lista.add(new Demanda("nombre 3","descripcion 3",R.drawable.persona));
+        lista.add(new Demanda("nombre 3","descripcion 3",R.drawable.persona));
+        lista.add(new Demanda("nombre 3","descripcion 3",R.drawable.persona));
+        lista.add(new Demanda("nombre 3","descripcion 3",R.drawable.persona));
+        lista.add(new Demanda("nombre 3","descripcion 3",R.drawable.persona));
+        lista.add(new Demanda("nombre 3","descripcion 3",R.drawable.persona));
+        lista.add(new Demanda("nombre 3","descripcion 3",R.drawable.persona));
+        lista.add(new Demanda("nombre 3","descripcion 3",R.drawable.persona));
     }
 
     // TODO: Rename method, update argument and hook method into UI event
@@ -92,16 +103,7 @@ public class ComunidadesFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
